d443f630dc2bff21d6f720b21c65231364ce70ee
DevanshTyagi04 <devansh.tyagi04@gmail.com>
2025-12-01 23:28:31 +0530

    Complete Auth UI(Sign-in Page)

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,59 @@
+import Image from "next/image"
+import Link from "next/link"
 
 const page = () => {
   return (
-    <div>
-      Auth
-    </div>
+    <main className="sign-in">
+      <aside className="testimonial">
+        <Link href="/">
+            <Image src="/assets/icons/logo.svg" alt="logo" width={32} height={32}/>
+            <h1>SnapCast</h1>
+        </Link>
+        <div className="description">
+            <section>
+                <figure>
+                    {Array.from({ length: 5 }).map((_,index) => (
+                      <Image src="/assets/icons/star.svg" alt="star" width={20} height={20} key={index}/>
+                    ))}
+                </figure>
+                <p>
+                  SnapCast makes screen recording easy. From quick walkthroughs to
+                  full presentations, it&apos;s fast, smooth, and shareable in seconds
+                </p>
+                <article>
+                    <Image src="/assets/images/jason.png" alt="Devansh" width={64} height={64} className="rounded-full"/>
+                    <div>
+                        <h2>
+                            Devansh Tyagi
+                        </h2>
+                        <p>
+                          Product Designer, SnapCast
+                        </p>
+                    </div>
+                </article>
+            </section>
+        </div>
+        <p>
+           &copy; SnapCast {(new Date().getFullYear())}
+        </p>
+      </aside>
+      <aside className="google-sign-in">
+          <section>
+              <Link href="/">
+                  <Image src="/assets/icons/logo.svg" alt="logo" width={40} height={40}/>
+                  <h1>SnapCast</h1>
+              </Link>
+              <p>
+                  Create and Share your very first <span>SnapCast video</span> in no time!
+              </p>
+              <button>
+                  <Image src="/assets/icons/google.svg" alt="google" width={22} height={22}/>
+                  <span>Sign in with Google</span>
+              </button>
+          </section>
+      </aside>
+      <div className="overlay"/>
+    </main>
   )
 }
 
