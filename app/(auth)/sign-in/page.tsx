a4b5829073713e8e07c28ca01dfbca13458998f0
DevanshTyagi04 <devansh.tyagi04@gmail.com>
2025-12-02 14:15:12 +0530

    Setup Authentication, Database and Middleware

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
 import Image from "next/image"
 import Link from "next/link"
 
 const page = () => {
+  const handleSignIn = async () => {
+      return await authClient.signIn.social({provider: 'google'})
+  }
   return (
     <main className="sign-in">
       <aside className="testimonial">
@@ -46,7 +52,7 @@ const page = () => {
               <p>
                   Create and Share your very first <span>SnapCast video</span> in no time!
               </p>
-              <button>
+              <button onClick={handleSignIn}>
                   <Image src="/assets/icons/google.svg" alt="google" width={22} height={22}/>
                   <span>Sign in with Google</span>
               </button>
